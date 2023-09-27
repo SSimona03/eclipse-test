f0c2c22f77724eb72a1fd466d58cb54e4e4dc146
ssimona03 <sstroe92@gmail.com>
2023-09-27 13:20:10 +0100

    added CDN for fontAwsome

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Head from "next/head";
+import Navbar from "@/components/NavBar";
 
 export default function Home() {
   return (
-   <main>
-    LG
-   </main>
-  )
+    <div>
+      <Navbar />
+      Home
+      <i className="fa-solid fa-cart-shopping"></i>
+    </div>
+  );
 }
